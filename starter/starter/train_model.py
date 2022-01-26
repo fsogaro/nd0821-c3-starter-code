3326301a445e8cce27ba7461067ca3ed1e219928
fsogaro <fm.sogaro@gmail.com>
2022-01-26 16:59:05 +0100

    Configure remote storage

--- starter/starter/train_model.py
+++ starter/starter/train_model.py
@@ -8,9 +8,14 @@ from ml.model import train_model, compute_model_metrics
 from ml.utils import save_pickle
 # Add the necessary imports for the starter code.
 # Add code to load in the data.
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
 
 def go():
-    data = pd.read_csv("data/census.csv")
+    data = pd.read_csv("../data/census_int.csv")
+    save_path = os.path.join("..", "model")
+    logging.info(f"*** loaded data: shape {data.shape} ***")
+    logging.info(f"*** loaded data: columns {data.columns} ***")
 
     # Optional enhancement, use K-fold cross validation instead of a train-test split.
     train, test = train_test_split(data, test_size=0.20)
@@ -27,31 +32,39 @@ def go():
     ]
     logging.info(f"*** processing train ***")
 
+
     X_train, y_train, encoder, lb = process_data(
-        train, categorical_features=cat_features, label="salary", training=True
+        train,
+        categorical_features=cat_features,
+        label="salary",
+        training=True
     )
     logging.info(f"*** processing test ***")
     X_test, y_test, _, _ = process_data(
-        train,
-        categorical_features=cat_features, label="salary", training=False,
-        encoder=encoder, lb=lb
+        test,
+        categorical_features=cat_features,
+        label="salary",
+        training=False,
+        encoder=encoder,
+        lb=lb
     )
 
     logging.info(f"*** saving encoders  ***")
 
-    save_pickle(encoder, "hot_encoder.pickle")
-    save_pickle(lb, "label_encoder.pickle")
+    save_pickle(encoder, os.path.join(save_path, "hot_encoder.pickle"))
+    save_pickle(lb, os.path.join(save_path, "label_encoder.pickle"))
 
     # Train and save a model.
     logging.info(f"*** training model ***")
     mdl = train_model(X_train, y_train)
     logging.info(f"*** save models  ***")
-    save_pickle(mdl, "trained_model.pickle")
+    save_pickle(mdl, os.path.join(save_path, "trained_model.pickle"))
 
     y_test_pred = mdl.predict(X_test)
     precision, recall, fbeta = compute_model_metrics(y_test, y_test_pred)
     logging.info(f"*** METRICS: ***")
-    logging.info(f"precision: {precision},\n recall: {recall},\n fbeta: {fbeta}")
+    logging.info(f"\n precision: {precision},\n recall: {recall},\n fbeta:"
+                 f" {fbeta}")
 
 
 if __name__ == "__main__":
