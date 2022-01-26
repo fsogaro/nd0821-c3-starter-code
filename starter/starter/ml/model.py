829141e583ab9d75f0725ea86d0a56de0a6b5ef6
fsogaro <fm.sogaro@gmail.com>
2022-01-26 23:17:26 +0100

    fixing dependencis and pytest

--- starter/starter/ml/model.py
+++ starter/starter/ml/model.py
@@ -4,9 +4,10 @@ import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from sklearn.model_selection import cross_val_score
-
-from ml.data import process_data, slice_data_on_category
 from collections import defaultdict
+
+from .data import process_data, slice_data_on_category
+
 logger = logging.getLogger()
 
 def train_model(X_train, y_train):
