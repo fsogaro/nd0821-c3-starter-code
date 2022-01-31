6ce672b6205afc4bf5edfc2d7a6ca808777e1ed8
fsogaro <fm.sogaro@gmail.com>
2022-01-31 16:16:59 +0100

    fixed dvc pull issue

--- main.py
+++ main.py
@@ -73,21 +73,6 @@ async def predict(data: CensusData):
     logging.info(f"looking for env info environ {os.environ} and seeing if "
                  f"need to call dvc")
 
-    # Heroku access to DVC data
-    print("looking for dir structure")
-    print(f"cwd : {os.getcwd()}, {os.path.isdir('.dvc')}")
-    print("***----***" * 50)
-    print(os.system("ls -a"))
-    print("***----***"*50)
-    print(os.system("ls -a ./model/"))
-
-    print(f"environ: {os.environ}")
-    # if "DYNO" in os.environ and os.path.isdir(".dvc"):
-    #     os.system("dvc config core.no_scm true")
-    #     # os.system("dvc remote add -d s3-bucket s3://udacity-mldevops-p3/dvcstore")
-    #     if os.system("dvc pull") != 0:
-    #         exit("dvc pull failed")
-    #     os.system("rm -r .dvc .apt/usr/lib/dvc")
     if "DYNO" in os.environ and os.path.isdir(".dvc"):
         os.system("dvc config core.no_scm true")
         os.system("dvc remote add -df s3-bucket s3://udacity-mldevops-p3/dvcstore")
