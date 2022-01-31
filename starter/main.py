3144c1607ec812dd926392bc479ddd54cdf497bd
fsogaro <fm.sogaro@gmail.com>
2022-01-31 11:36:04 +0100

    path to local .dvc folder not in subfolder

--- starter/main.py
+++ starter/main.py
@@ -2,6 +2,7 @@
 
 # Import Union since our Item object will have tags that can be strings or a list.
 import os
+import logging
 from typing import Optional
 
 import pandas as pd
@@ -13,6 +14,8 @@ from starter.ml.utils import load_pickle
 
 app = FastAPI()
 
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
 
 @app.get("/")
 async def welcome_msg():
@@ -44,6 +47,7 @@ class CensusData(BaseModel):
 # This allows sending of data (our TaggedItem) via POST to the API.
 @app.post("/predict")
 async def predict(data: CensusData):
+    logging.info("calling post method...")
     cat_features = [
         "workclass",
         "education",
@@ -54,21 +58,27 @@ async def predict(data: CensusData):
         "sex",
         "native-country",
     ]
+    logging.info(f"looking for env info environ {os.environ} and seeing if "
+                 f"need to call dvc")
 
     # Heroku access to DVC data
-    if "DYNO" in os.environ and os.path.isdir(".dvc"):
+    if "DYNO" in os.environ and os.path.isdir("../.dvc"):
         os.system("dvc config core.no_scm true")
+        # os.system("dvc remote add -d s3-bucket s3://udacity-mldevops-p3/dvcstore")
         if os.system("dvc pull") != 0:
             exit("dvc pull failed")
         os.system("rm -r .dvc .apt/usr/lib/dvc")
+
 
     cwd = os.getcwd()
     mdl = load_pickle(os.path.join(cwd, "model", "trained_model.pickle"))
     encoder = load_pickle(os.path.join(cwd, "model", "hot_encoder.pickle"))
     lb = load_pickle(os.path.join(cwd, "model", "label_encoder.pickle"))
+    logging.info(f"loaded models and transformers")
 
     df = pd.DataFrame(jsonable_encoder(data.dict(by_alias=True)), index=[0])
     y = inference_new_data(df, mdl, encoder, lb, cat_features=cat_features)
+    logging.info(f" inference done")
 
     return {"data": data, "prediction": y.tolist()[0]}
 
